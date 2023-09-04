70810e51e289b0b5246388e8a1d253e500cb79f7
Alla Rakhimova <alla.rakhimova103@gmail.com>
2023-09-04 21:37:15 +0300

    add Profile image

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,25 +1,35 @@
-import * as constants from '../../constants';
-import profileImage from '../../assets/images/image-profile-desktop.webp';
-import './HeroSection.scss';
+import * as constants from "../../constants";
+import profileImageDesktop from "../../assets/images/image-profile-desktop.webp";
+import profileImageTablet from "../../assets/images/image-profile-tablet.webp";
+import profileImageMobile from "../../assets/images/image-profile-mobile.webp";
+import "./HeroSection.scss";
 
 const HeroSection = () => {
-
   console.log(constants);
   return (
-    <div className="hero__content-wrapper">
-      <div className="hero__content">
-        <h1>{constants.HERO_TITLE}</h1>
-        <h2>{constants.HERO_DESCRIPTION}</h2>
-        <button className="hero__contact-button">{constants.HERO_BUTTON}</button>
-      </div>
+    <>
+      <div className="hero__content-wrapper container">
+        <div className="hero__content">
+          <h1>
+            Nice to meet you! I&apos;m <span>Adam Keyes</span>.
+          </h1>
+          <h2>{constants.HERO_DESCRIPTION}</h2>
+          <button className="hero__contact-button">
+            {constants.HERO_BUTTON}
+          </button>
+        </div>
 
-      <img
-        className="hero__profile-image"
-        //TODO change image depends on the device
-        src={profileImage}
-        alt="profile-image"
-      />
-    </div>
+        <picture>
+          <source media="(min-width: 62.5em)" srcSet={profileImageDesktop} />
+          <source media="(min-width: 37.5em)" srcSet={profileImageTablet} />
+          <img
+            className="hero__profile-image"
+            src={profileImageMobile}
+            alt="profile image"
+          />
+        </picture>
+      </div>
+    </>
   );
 };
 
