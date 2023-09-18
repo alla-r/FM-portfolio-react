1d9adc067d93217fa98cf1924027fea89774d3b5
Alla Rakhimova <alla.rakhimova103@gmail.com>
2023-09-18 16:40:23 +0300

    move Form to separate component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,56 +1,9 @@
-import { useFormik } from 'formik';
 import { FORM_DATA } from '../../constants';
 import Rings from '../../assets/images/pattern-rings.svg';
-import InputField from './InputField';
-import { CONTACT_FORM_SCHEMA } from '../../validationSchema';
+import Form from './Form';
 import './ContactForm.scss';
 
 const ContactForm = () => {
-  const onSubmitCallback = (values, actions) => {
-    console.log(values);
-    actions.resetForm();
-    // TODO: sent data
-  };
-
-  const {
-    values,
-    errors,
-    touched,
-    isSubmitting,
-    handleSubmit,
-    handleChange,
-    handleBlur,
-    setFieldTouched,
-  } = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      message: '',
-    },
-    validationSchema: CONTACT_FORM_SCHEMA,
-    onSubmit: onSubmitCallback,
-  });
-
-  const fields = FORM_DATA.data.map(({ id, label, placeholder, type, inputType }) => {
-    return (
-      <InputField
-        key={id}
-        label={label}
-        placeholder={placeholder}
-        type={type}
-        inputType={inputType}
-        id={id}
-        value={values[id]}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        onFocus={() => setFieldTouched(id, false)}
-        isError={errors[id] && touched[id]}
-        errorText={errors[id]}
-      />
-    );
-  });
-
-  // TODO: rings design + move form logic to a component
   return (
     <section id="contact-form" className="contact-form">
       <div className="container">
@@ -59,12 +12,7 @@ const ContactForm = () => {
             <h2 className="main-title">{FORM_DATA.title}</h2>
             <p className="contact-form__description">{FORM_DATA.description}</p>
           </div>
-          <form className="form" onSubmit={handleSubmit}>
-            {fields}
-            <button disabled={isSubmitting} type="submit" className="main-button">
-              {FORM_DATA.buttonText}
-            </button>
-          </form>
+          <Form />
         </div>
         <img className="contact-form__rings" src={Rings} alt="rings" />
       </div>
---
+++ src/components/ContactForm/Form/Form.jsx
@@ -0,0 +1,62 @@
+import { useFormik } from 'formik';
+import { CONTACT_FORM_SCHEMA } from '../../../validationSchema';
+import { FORM_DATA } from '../../../constants';
+import InputField from '../InputField';
+import './Form.scss';
+
+const Form = () => {
+  const onSubmitCallback = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+    // TODO: sent data
+  };
+
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    setFieldTouched,
+  } = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema: CONTACT_FORM_SCHEMA,
+    onSubmit: onSubmitCallback,
+  });
+
+  const fields = FORM_DATA.data.map(({ id, label, placeholder, type, inputType }) => {
+    return (
+      <InputField
+        key={id}
+        label={label}
+        placeholder={placeholder}
+        type={type}
+        inputType={inputType}
+        id={id}
+        value={values[id]}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onFocus={() => setFieldTouched(id, false)}
+        isError={errors[id] && touched[id]}
+        errorText={errors[id]}
+      />
+    );
+  });
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      {fields}
+      <button disabled={isSubmitting} type="submit" className="main-button">
+        {FORM_DATA.buttonText}
+      </button>
+    </form>
+  );
+};
+
+export default Form;
