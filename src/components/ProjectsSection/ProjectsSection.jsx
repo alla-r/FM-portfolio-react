88fa131800957647e6432cc04d19c9ade465f3af
Alla Rakhimova <alla.rakhimova103@gmail.com>
2023-09-18 23:53:33 +0300

    add Desktop styles for Projects section

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -2,12 +2,18 @@ import { PROJECTS_DATA, PROJECTS_BUTTONS_TEXT } from '../../constants';
 import './ProjectsSection.scss';
 
 const ProjectsSection = () => {
-  // TODO: DESKTOP version
   const projects = PROJECTS_DATA.data.map(
     ({ id, title, imgSrcSmall, imgSrcLarge, technologies }) => {
       return (
         <div key={id} className="projects__item">
-          <img className="projects__thumbnail" src={imgSrcSmall} alt="thumbnail 1" />
+          <picture className="projects__picture">
+            <source media="(min-width: 63.5em)" srcSet={imgSrcLarge} />
+            <img
+              className="projects__thumbnail"
+              src={imgSrcSmall}
+              alt={`thumbnail for ${id} project`}
+            />
+          </picture>
           <div className="projects__title">{title}</div>
           <div className="projects__technologies">
             {technologies.map((el, i) => (
@@ -16,9 +22,11 @@ const ProjectsSection = () => {
               </p>
             ))}
           </div>
-          <div className="projects__buttons">
-            <button className="main-button">{PROJECTS_BUTTONS_TEXT.VIEW_PROJECT}</button>
-            <button className="main-button">{PROJECTS_BUTTONS_TEXT.VIEW_CODE}</button>
+          <div className="projects__overlay">
+            <div className="projects__buttons">
+              <button className="main-button">{PROJECTS_BUTTONS_TEXT.VIEW_PROJECT}</button>
+              <button className="main-button">{PROJECTS_BUTTONS_TEXT.VIEW_CODE}</button>
+            </div>
           </div>
         </div>
       );
