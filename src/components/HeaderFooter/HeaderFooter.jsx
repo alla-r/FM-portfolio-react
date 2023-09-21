e0a6819e1f23e4f8433a83e5fe3503338536dbe3
Alla Rakhimova <alla.rakhimova103@gmail.com>
2023-09-21 23:49:25 +0300

    run prettier and fix accessibility

--- src/components/HeaderFooter/HeaderFooter.jsx
+++ src/components/HeaderFooter/HeaderFooter.jsx
@@ -5,15 +5,15 @@ import './HeaderFooter.scss';
 const HeaderFooter = ({ isHeader = true }) => {
   const links = HEADER_NAV_LINK_CONFIG.map(({ id, url, title, width, height, fillRule, d }) => {
     return (
-      <a key={`key-${id}`} href={url} target="_blank" className="header__social-link" rel="noreferrer">
+      <a key={id} href={url} target="_blank" className="header__social-link" rel="noreferrer">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width={width}
           height={height}
           role="img"
-          aria-labelledby={id}
+          aria-labelledby={isHeader ? `header-${id}` : `footer-${id}`}
         >
-          <title id={id}>{title}</title>
+          <title id={isHeader ? `header-${id}` : `footer-${id}`}>{title}</title>
           <path fill="#FFF" fillRule={fillRule} d={d} />
         </svg>
       </a>
@@ -22,7 +22,7 @@ const HeaderFooter = ({ isHeader = true }) => {
 
   const content = (
     <div className="container">
-      <nav aria-label={isHeader ? "Header" : "Footer"} className="header__nav">
+      <nav aria-label={isHeader ? 'Header' : 'Footer'} className="header__nav">
         <a href="#" className="header__logo">
           {HEADER_LOGO_TEXT}
           <span className="visually-hidden">(to home page)</span>
