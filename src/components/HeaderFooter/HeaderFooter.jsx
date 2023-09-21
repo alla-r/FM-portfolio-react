f0a56cee470c7a5f82fca2bd8ed911d4deeec1b0
Alla Rakhimova <alla.rakhimova103@gmail.com>
2023-09-21 23:40:32 +0300

    add unique landmarks

--- src/components/HeaderFooter/HeaderFooter.jsx
+++ src/components/HeaderFooter/HeaderFooter.jsx
@@ -22,7 +22,7 @@ const HeaderFooter = ({ isHeader = true }) => {
 
   const content = (
     <div className="container">
-      <nav role="navigation" aria-label="Social medias" className="header__nav">
+      <nav aria-label={isHeader ? "Header" : "Footer"} className="header__nav">
         <a href="#" className="header__logo">
           {HEADER_LOGO_TEXT}
           <span className="visually-hidden">(to home page)</span>
